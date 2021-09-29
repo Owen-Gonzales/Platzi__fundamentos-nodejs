9ed6ae089fab7ad7c6370e232a3641920d64895a
Owen Gonzales <26owenkgo@gmail.com>
2021-09-28 20:11:29 -0500

    Types of console methods

---
+++ modules/console.js
@@ -0,0 +1,32 @@
+let pokemons = {
+  pikachu: {
+    type: 'Electric',
+    level: 5
+  },
+  charmander: {
+    type: 'Fire',
+    level: 15
+  }
+}
+
+console.group('Kind of informative console')
+  console.log('Something normal')
+  console.info('Something info')
+  console.error('something error')
+  console.warn('something warn')
+console.groupEnd('Kind of informative console')
+
+console.group('Console for objects')
+  console.table(pokemons)
+console.groupEnd('Console for objects')
+
+console.group('Console.count')
+for(let i = 0; i < 3; i++) {
+  console.count('Times')
+}
+  console.count('Times')
+  console.countReset('Times')
+  console.count('Times')
+  console.count('Times')
+console.groupEnd('Console.count')
+
